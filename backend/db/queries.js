e9c9929de9136d22bd815cab0f7221b2aa62ca8b
C4Q <c4q@C4Qs-MacBook-Pro-80.local>
2018-03-19 11:38:01 -0400

    created 4 more new get routes in backend users.js, 4 more queries as well

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -75,6 +75,55 @@ function getRecipeComments(req, res, next) {
     });
 }
 
+function getAllUsers(req, res, next) {
+  db.any(`SELECT USER_id, username, email, first_name, last_name
+          FROM users;`)
+          .then(data => {
+            res.json(data);
+          })
+          .catch(error => {
+            res.json(error);
+          });
+}
+
+function getAllResipes(req, res, next) {
+  db.any(`SELECT *
+          FROM recipes;`)
+          .then(data => {
+            res.json(data);
+          })
+          .catch(error => {
+            res.json(error);
+          });
+}
+
+function getAllResipesByUserID(req, res, next) {
+  db.any(`SELECT *
+          FROM recipes
+          WHERE user_id=${req.params.userID};`)
+          .then(data => {
+            res.json(data);
+          })
+          .catch(error => {
+            res.json(error);
+          });
+}
+
+function getAllFollowersRecipes(req, res, next) {
+  db.any(`SELECT users.user_id, recipe_name, recipe_id, recipe, img, isvegeterian, isvegan, recipe_timestamp
+          FROM users
+          INNER JOIN followings ON(users.user_id=followings.follower_id)
+          INNER JOIN recipes
+          ON(users.user_id=recipes.user_id)
+          WHERE followee_id=${req.params.userID};`)
+          .then(data => {
+            res.json(data);
+          })
+          .catch(error => {
+            res.json(error);
+          });
+}
+
 /*-------------------------------POST Request----------------------------------*/
 
 function registerUser(req, res, next) {
@@ -317,5 +366,9 @@ module.exports = {
   loginUser,
   editUser,
   editRecipe,
-  editRecipeComment
+  editRecipeComment,
+  getAllUsers,
+  getAllResipes,
+  getAllResipesByUserID,
+  getAllFollowersRecipes,
 };
