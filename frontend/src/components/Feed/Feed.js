c0e11f2448470b20cc08da90c0ac9b55ebbfba20
Xavier <xaviermunroe@ac.c4q.nyc>
2018-03-19 14:46:29 -0400

    added List folder, List.js file and edited Cookbook.js so it no longer fetches the most favorited recipes. Cookbook.js now only gets all users, all recipes, the info of a logged in user if they are logged in.

--- frontend/src/components/Feed/Feed.js
+++ frontend/src/components/Feed/Feed.js
@@ -1,1 +1,17 @@
 import React from "react"
+
+class Feed extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <div>
+        Feed Feed Feed
+      </div>
+    )
+  }
+}
+
+export default Feed 
