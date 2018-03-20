9c818c8012b432fb65cd6830346c7b1d0cc92d06
shaedonBlackman <sblackman89@gmail.com>
2018-03-20 19:32:30 -0400

    test2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {Route, Link, Switch} from 'react-router-dom';
-import AddRecipe from './components/SingleRecipe/AddRecipe'
 
 import logo from './logo.svg';
 import './App.css';
@@ -9,17 +8,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Switch>
-        <Route exact path='/' component={AddRecipe}/>
-        </Switch>
-        {/* <header className="App-header">
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <ShowAllComments comments={['red','green','blue']}/> */}
+        <ShowAllComments comments={['red','green','blue']}/>
       </div>
     );
   }
