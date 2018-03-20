69ba7f804e97b67d05b3ca852ac6723ff4ec62ad
shaedonBlackman <sblackman89@gmail.com>
2018-03-20 19:26:44 -0400

    added AddRecipe.js and made correction to Comments.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Route, Link, Switch} from 'react-router-dom';
-import ShowAllComments from './components/Comments/Comments'
+import AddRecipe from './components/SingleRecipe/AddRecipe'
 
 import logo from './logo.svg';
 import './App.css';
@@ -9,14 +9,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+      <Switch>
+        <Route exact path='/' component={AddRecipe}/>
+        </Switch>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <ShowAllComments comments={['red','green','blue']}/>
+        <ShowAllComments comments={['red','green','blue']}/> */}
       </div>
     );
   }
