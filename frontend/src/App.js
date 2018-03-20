734ef3a121a0c203655edff391523265a89e1391
erostark <eioncontaste@ac.c4q.nyc>
2018-03-20 19:35:12 -0400

    Making styling for featured posts on landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Route, Link, Switch} from 'react-router-dom';
-import Home from './components/Home/Home'
+import ShowAllComments from './components/Comments/Comments'
 
 import logo from './logo.svg';
 import './App.css';
@@ -9,9 +9,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Switch>
-        <Route exact path='/' component={Home} />
-        </Switch>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <ShowAllComments comments={['red','green','blue']}/>
       </div>
     );
   }
