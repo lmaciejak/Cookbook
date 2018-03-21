22ae478538269abf29cfc1db3ad81165b7849fb0
C4Q <c4q@C4Qs-MBP-80.home>
2018-03-21 17:21:07 -0400

    added 2 more backend routes with queries and in Recipe.js coded SingleRecipe stateless function

--- frontend/src/components/SingleRecipe/Recipe.js
+++ frontend/src/components/SingleRecipe/Recipe.js
@@ -1,1 +1,34 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SingleRecipe = (props) => {
+
+  const { favorites_count, username,
+      recipe_name, recipe, img,
+      isvegeterian, isvegan,
+      recipe_timestamp, ingredients } = props;
+
+  return (
+    <div>
+      <h1>Name {recipe_name}</h1>
+      <img src={img} alt="recipe_image" />
+      <p>Direction {recipe}</p>
+      <ul type="none">Ingredient
+        {
+          ingredients.map( (ingredient) => {
+            return <li key={Math.random()}>{ingredient.amount}{" "}{ingredient.name}</li>
+          })
+        }
+      </ul>
+      <h6>Chef {username}</h6>
+      <p>
+        Vegeterian {isvegeterian}{" "}
+        Vegan {isvegan}{" "}
+        Favorites {favorites_count}{" "}
+        uploaded {recipe_timestamp}{" "}
+      </p>
+      <Link to="/">Back</Link>
+    </div>
+  )
+}
+export default SingleRecipe;
