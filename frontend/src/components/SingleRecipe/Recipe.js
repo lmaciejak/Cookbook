cd62bc11bbdcec33ddee8b70f0b7105f1bcddaa5
C4Q <c4q@C4Qs-MBP-80.home>
2018-03-23 00:18:04 -0400

    SingleRecipe is fetching all appropriate informations and rendering it

--- frontend/src/components/SingleRecipe/Recipe.js
+++ frontend/src/components/SingleRecipe/Recipe.js
@@ -1,34 +1,96 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-const SingleRecipe = (props) => {
+class SingleRecipe extends React.Component {
 
-  const { favorites_count, username,
-      recipe_name, recipe, img,
-      isvegeterian, isvegan,
-      recipe_timestamp, ingredients } = props;
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites_count: "",
+      username: "",
+      recipe_name: "",
+      recipe: "",
+      img: "",
+      isvegeterian: "",
+      isvegan: "",
+      comments: "",
+      ingredients: []
+    }
+  }
 
-  return (
-    <div>
-      <h1>Name {recipe_name}</h1>
-      <img src={img} alt="recipe_image" />
-      <p>Direction {recipe}</p>
-      <ul type="none">Ingredient
-        {
-          ingredients.map( (ingredient) => {
-            return <li key={Math.random()}>{ingredient.amount}{" "}{ingredient.name}</li>
-          })
-        }
-      </ul>
-      <h6>Chef {username}</h6>
-      <p>
-        Vegeterian {isvegeterian}{" "}
-        Vegan {isvegan}{" "}
-        Favorites {favorites_count}{" "}
-        uploaded {recipe_timestamp}{" "}
-      </p>
-      <Link to="/">Back</Link>
-    </div>
-  )
+  componentWillMount() {
+    axios
+      .get(`/users/singlerecipe/${this.props.user.recipeID}`)
+      .then( (res) => {
+        this.setState({
+          favorites_count: res.data[0].favorites_count,
+          username: res.data[0].username,
+          recipe_name: res.data[0].recipe_name,
+          recipe: res.data[0].recipe,
+          img: res.data[0].img,
+          isvegeterian: res.data[0].isvegeterian,
+          isvegan: res.data[0].isvegan
+        })
+      })
+      .catch( (err) => {
+        console.log(err);
+      })
+    axios
+      .get(`/users/getingredients/${this.props.user.recipeID}`)
+      .then( (res) => {
+        this.setState({
+          ingredients: res.data
+        })
+      })
+      .catch( (error) => {
+        console.log(error);
+      })
+    axios
+      .get(`/users/comment/${this.props.user.recipeID}`)
+      .then( (res) => {
+        this.setState({
+          comments: res.data
+        })
+      })
+      .catch( (error) => {
+        console.log(error);
+      })
+  }
+
+  render() {
+    const { favorites_count, username,
+            recipe_name, recipe, img,
+            isvegeterian, isvegan,
+            ingredients, comments } = this.state;
+    return (
+      <div>
+        <h1>Name {recipe_name}</h1>
+        <img src={img} alt="recipe_image" />
+        <p>Direction {recipe}</p>
+        <ul type="none">Ingredient
+          {
+            ingredients? ingredients.map( (ingredient) => {
+              return <li key={Math.random()}>{ingredient.amount}{" "}{ingredient.name}</li>
+            }) : "There are no any ingredients"
+          }
+        </ul>
+        <h6>Chef {username}</h6>
+        <p>
+          Vegeterian {isvegeterian}{" "}
+          Vegan {isvegan}{" "}
+          Favorites {favorites_count}{" "}
+        </p>
+        <ul type="none">Comments
+          {
+            comments? comments.map( (comment) => {
+              return <li key={Math.random()}>{comment.comment}</li>
+            }) : "There are no any comments"
+          }
+        </ul>
+        <Link to={`/cb/profile/${this.props.user.username}`}>Back</Link>
+      </div>
+    )
+  }
 }
 export default SingleRecipe;
