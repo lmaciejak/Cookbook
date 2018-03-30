a65d805db439622beed778f1e28f807ddcb2d08b
Umedjon Ibrohimov <umedjonibrohimov@ac.c4q.nyc>
2018-03-30 16:31:26 -0400

    Update UserProfile.js
    console.log() was deleted straight from master

--- frontend/src/components/Profile/UserProfile.js
+++ frontend/src/components/Profile/UserProfile.js
@@ -259,7 +259,6 @@ class UserProfile extends React.Component {
 
   renderAllUserRecipes = () => {
     const { allusersRecipes } = this.state;
-    console.log("allusersRecipesSSSSSSS: ", allusersRecipes);
     return (
       <div className="userProfileAllRecipes">
         {allusersRecipes
