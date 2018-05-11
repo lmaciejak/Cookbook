fb65838bcfa7c4ec8d59df8764679f4157a132b7
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2018-05-11 17:12:19 -0400

    add assets

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Lexus
+# Cookbook
+
+![Alt text](./assets/cookbook-landing.png?raw=true "Landing Page")
--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,6 @@
 var pgp = require("pg-promise")({});
-var connectionString = process.env.DATABASE_URL;
-// var connectionString = "postgres://localhost/cookbookdb";
+// var connectionString = process.env.DATABASE_URL;
+var connectionString = "postgres://localhost/cookbookdb";
 var db = pgp(connectionString);
 
 module.exports = db;
